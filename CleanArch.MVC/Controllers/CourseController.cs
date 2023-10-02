6b7f7011ff79be0898faff59ab7996373f764deb
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-10-02 11:43:48 +0530

    CourseController is Added

---
+++ CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
